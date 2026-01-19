a42d5369b4f096382dc0f420f246897a2fec9bcf
ASUS <Sauravkumar5july@gmail.com>
2026-01-19 18:17:58 +0530

    Mix of single & Multi Line comment.

---
+++ src/ex_02_Java_Basics/Lab005_Mix_Single_Multi.java
@@ -0,0 +1,15 @@
+package ex_02_Java_Basics;
+
+public class Lab005_Mix_Single_Multi {
+    /**
+     *Author: Pramod
+     * Course:ATB15X
+     * Learning:Java
+     **/
+    public static void main(String[] args) {
+        //Method Implementation
+
+        //System.out.println("All is well");
+        //System.out.println("Work Hard");
+    }
+}
