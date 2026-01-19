ed43e877b78339a80a1fd951db34d3ffe7499d10
ASUS <Sauravkumar5july@gmail.com>
2026-01-19 15:11:21 +0530

    Some Changes in Single Line comments Program.

--- src/ex_02_Java_Basics/Lab003_comment.java
+++ src/ex_02_Java_Basics/Lab003_comment.java
@@ -3,7 +3,11 @@ package ex_02_Java_Basics;
 public class Lab003_comment {
     public static void main(String[] args) {
         //This is a comment.
-        //This is a Single line comment.
-        //This Code will not execute.
+        //This is a comment.
+        //This is a comment.
+        //This is a comment.
+        //This is a comment.
+        System.out.println("This is Single line comment");
+
     }
 }
