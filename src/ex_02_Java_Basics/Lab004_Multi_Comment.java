615cc5640d0d1aa5147c3496f04885fd258b7cda
ASUS <Sauravkumar5july@gmail.com>
2026-01-19 18:16:35 +0530

    Multi Line comment

---
+++ src/ex_02_Java_Basics/Lab004_Multi_Comment.java
@@ -0,0 +1,16 @@
+package ex_02_Java_Basics;
+/**
+ *Author: Pramod
+ * Course:ATB15X
+ * Learning:Java
+ **/
+public class Lab004_Multi_Comment {
+    /**
+     *we are inside the class
+     **/
+    public static void main(String[] args){
+        /**
+         * we are inside the program
+         **/
+    }
+}
