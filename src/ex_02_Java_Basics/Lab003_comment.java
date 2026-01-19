663a70584b6e494bb501d434165dbf994ab9a028
ASUS <Sauravkumar5july@gmail.com>
2026-01-19 15:03:00 +0530

    my third java program for learning single comments.

---
+++ src/ex_02_Java_Basics/Lab003_comment.java
@@ -0,0 +1,9 @@
+package ex_02_Java_Basics;
+
+public class Lab003_comment {
+    public static void main(String[] args) {
+        //This is a comment.
+        //This is a Single line comment.
+        //This Code will not execute.
+    }
+}
